06968ac422e7f62730b698b048de5e09d7600013
Jeff Allen <jeffallen89@gmail.com>
2023-03-06 15:35:20 -0500

    svelte skeleton

---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+
+	import Top from "./lib/TopSofC.svelte";
+	import './styles.css';
+
+</script>
+
+
+
+<svelte:head>
+
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto&family=Source+Serif+Pro&display=swap"
+		rel="stylesheet"
+	/>
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/> 
+
+	<link 
+		href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;800&display=swap" rel="stylesheet"
+	/>
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1, minimum-scale=1"
+	/>
+
+	<link href='https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css' rel='stylesheet' />
+
+    <!-- <title>Tracking Gentle Density in Toronto</title>
+    <meta name="description" content="Visualizing 10 years (2013 to 2022) of Secondary Suite and Laneway / Garden Suite building permits in Toronto">
+    <meta name="author" content="Jeff Allen">
+
+	<meta property="og:title" content="Tracking Gentle Density in Toronto" />
+    <meta property="og:description" content="Visualizing 10 years (2013 to 2022) of Secondary Suite and Laneway / Garden Suite building permits in Toronto" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://schoolofcities.github.io/gentle-density/toronto-building-permits" />
+    <meta property="og:image" content="https://raw.githubusercontent.com/schoolofcities/gentle-density/main/static/web-card.png" />
+    <meta property="og:locale" content="en_CA">
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="https://schoolofcities.github.io/gentle-density/toronto-building-permits" />
+    <meta name="twitter:creator" content="@JeffAllenMaps" />
+    <meta name="twitter:title" content="Tracking Gentle Density in Toronto" />
+    <meta name="twitter:description" content="Visualizing 10 years (2013 to 2022) of Secondary Suite and Laneway / Garden Suite building permits in Toronto" />
+    <meta name="twitter:image" content="https://raw.githubusercontent.com/schoolofcities/gentle-density/main/static/web-card.png" />  -->
+
+</svelte:head>
+
+
+
+<Top/>
+
+<main>
+
+	meow meow meow
+
+</main>
