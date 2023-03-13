c629caac9c07e31b3bd81c3ced2b94418464ff6d
Jeff Allen <jeffallen89@gmail.com>
2023-03-13 10:22:07 -0400

    2017 tcra land cover layer to tiles

--- src/routes/lib/Map.svelte
+++ src/routes/lib/Map.svelte
@@ -294,19 +294,19 @@
         
         <button class:selected="{year === '1939'}"  on:click={() => yearSelect("1939")}>1939</button>
         
-        <button class:selected="{year === '1947'}" on:click={() => yearSelect("1947")}>1947</button>
+        <!-- <button class:selected="{year === '1947'}" on:click={() => yearSelect("1947")}>1947</button> -->
 
         <button class:selected="{year === '1954'}" on:click={() => yearSelect("1954")}>1954</button>
 
-        <button class:selected="{year === '1966'}"  on:click={() => yearSelect("1966")}>1966</button>
+        <!-- <button class:selected="{year === '1966'}"  on:click={() => yearSelect("1966")}>1966</button> -->
         
         <button class:selected="{year === '1973'}" on:click={() => yearSelect("1973")}>1973</button>
 
-        <button class:selected="{year === '1982'}" on:click={() => yearSelect("1982")}>1982</button>
+        <!-- <button class:selected="{year === '1982'}" on:click={() => yearSelect("1982")}>1982</button> -->
 
-        <button class:selected="{year === '1994'}"  on:click={() => yearSelect("1994")}>1994</button>
+        <!-- <button class:selected="{year === '1994'}"  on:click={() => yearSelect("1994")}>1994</button> -->
         
-        <button class:selected="{year === '2006'}" on:click={() => yearSelect("2006")}>2006</button>
+        <!-- <button class:selected="{year === '2006'}" on:click={() => yearSelect("2006")}>2006</button> -->
 
         <button class:selected="{year === '2017'}" on:click={() => yearSelect("2017")}>2017</button>
 
