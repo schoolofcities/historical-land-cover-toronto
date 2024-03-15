bbc7ec8723f963ae6796e34cad234d35f531ab9a
Jeff Allen <jeffallen89@gmail.com>
2024-03-15 15:30:06 -0400

    tidying up code, ribbon adjust items

--- src/lib/MapNeighbourhood.svelte
+++ src/lib/MapNeighbourhood.svelte
@@ -1,189 +1,189 @@
 <script>
 
-    import './ol.css';
-    import '../assets/styles.css';
+	import './ol.css';
+	import '../assets/styles.css';
 
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
-    import notToronto from '../assets/ref/not-toronto.geo.json';
-    import majorStreets from '../assets/ref/major-roads.geo.json';
-    import labelStreets from '../assets/ref/major-streets-split.geo.json';
-    import sixLabels from '../assets/ref/six-labels.geo.json';
-    import shoreline from '../assets/ref/shoreline.geo.json';
-    import neighbourhoods from '../assets/ref/neighbourhoods.geo.json';
+	import notToronto from '../assets/ref/not-toronto.geo.json';
+	import majorStreets from '../assets/ref/major-roads.geo.json';
+	import labelStreets from '../assets/ref/major-streets-split.geo.json';
+	import sixLabels from '../assets/ref/six-labels.geo.json';
+	import shoreline from '../assets/ref/shoreline.geo.json';
+	import neighbourhoods from '../assets/ref/neighbourhoods.geo.json';
 
-    import {Map, View} from 'ol';
-    import XYZ from 'ol/source/XYZ';
+	import {Map, View} from 'ol';
+	import XYZ from 'ol/source/XYZ';
 	import TileLayer from 'ol/layer/Tile';
-    import {useGeographic} from 'ol/proj';
-    import {get as getProjection} from 'ol/proj';
+	import {useGeographic} from 'ol/proj';
+	import {get as getProjection} from 'ol/proj';
 	import {getWidth} from 'ol/extent';
 
 
-    import {Fill, Stroke, Style, Text} from 'ol/style';
+	import {Fill, Stroke, Style, Text} from 'ol/style';
 
-    import GeoJSON from 'ol/format/GeoJSON';
+	import GeoJSON from 'ol/format/GeoJSON';
 	import VectorLayer from 'ol/layer/Vector';
 	import VectorSource from 'ol/source/Vector';
 
 	import {defaults as defaultControls} from 'ol/control';
-    import {defaults as defaultInteractions} from 'ol/interaction';
+	import {defaults as defaultInteractions} from 'ol/interaction';
 	import {ScaleLine} from 'ol/control';
-    
+	
 
-    let map = 0;
-    let load = 0;
-    let year = "1947";
-    let streets = "on";
+	let map = 0;
+	let load = 0;
+	let year = "1947";
+	let streets = "on";
 
-    var neighbourhoodOpacity = 0.8;
-
-
-    // local streets layer
-    var smallStreetsSource = new XYZ({
-        url: "./streets-local/{z}/{x}/{y}.png"
-    });
-    var smallStreetsLayer = new TileLayer({
-        minZoom: 14,
-        maxZoom: 15,
-        opacity: 1,
-        source: smallStreetsSource
-    });
+	var neighbourhoodOpacity = 0.8;
 
 
+	// local streets layer
+	var smallStreetsSource = new XYZ({
+		url: "./streets-local/{z}/{x}/{y}.png"
+	});
+	var smallStreetsLayer = new TileLayer({
+		minZoom: 14,
+		maxZoom: 15,
+		opacity: 1,
+		source: smallStreetsSource
+	});
 
-    // not Toronto layer - include a background pattern with canvas
 
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    canvas.width = 5;
-    canvas.height = 5;
 
-    context.fillStyle = 'white'; // Fill color
-    context.fillRect(0, 0, 5, 5);
-    context.strokeStyle = '#D0D1C9'; // Stroke color
-    context.lineWidth = 1;
-    context.beginPath();
-    context.moveTo(2, 3);
-    context.lineTo(3, 2);
-    context.stroke();
+	// not Toronto layer - include a background pattern with canvas
 
-    const pattern = context.createPattern(canvas, 'repeat');
+	const canvas = document.createElement('canvas');
+	const context = canvas.getContext('2d');
+	canvas.width = 5;
+	canvas.height = 5;
 
-    
+	context.fillStyle = 'white'; // Fill color
+	context.fillRect(0, 0, 5, 5);
+	context.strokeStyle = '#D0D1C9'; // Stroke color
+	context.lineWidth = 1;
+	context.beginPath();
+	context.moveTo(2, 3);
+	context.lineTo(3, 2);
+	context.stroke();
 
-    // streets layer 2022ish
-    var features = new GeoJSON().readFeatures(majorStreets, {
+	const pattern = context.createPattern(canvas, 'repeat');
+
+	
+
+	// streets layer 2022ish
+	var features = new GeoJSON().readFeatures(majorStreets, {
 		});
 	var vectorSource = new VectorSource({
 		features
 	});
 	var textStyle = new Style({
-            text: new Text({
-                declutter: true,
-                font: 'bold 12px "Roboto", "Arial Unicode MS", "sans-serif"',
-                placement: 'line',
-                scale: 1,
-                fill: new Fill({
-                    color: '#1E3765',
-                }),
-                stroke: new Stroke({
-                    color: 'white',
-                    width: 2
-                })
-            }),
-            stroke: new Stroke({
-                color: '#1E3765',
-                width: 1
-            })
+			text: new Text({
+				declutter: true,
+				font: 'bold 12px "Roboto", "Arial Unicode MS", "sans-serif"',
+				placement: 'line',
+				scale: 1,
+				fill: new Fill({
+					color: '#1E3765',
+				}),
+				stroke: new Stroke({
+					color: 'white',
+					width: 2
+				})
+			}),
+			stroke: new Stroke({
+				color: '#1E3765',
+				width: 1
+			})
 		});
 	var streetLayer = new VectorLayer({
 		declutter: true,
-        opacity: 0.75,
+		opacity: 0.75,
 		source: vectorSource,
 		style: function (feature) {
 			textStyle.getText().setText(feature.get('n'));
-        	return textStyle;
-      	},		
+			return textStyle;
+	  	},		
 	});
 
-    // streets layer labels 2022ish
-    var features = new GeoJSON().readFeatures(labelStreets, {
+	// streets layer labels 2022ish
+	var features = new GeoJSON().readFeatures(labelStreets, {
 		});
 	var vectorSource = new VectorSource({
 		features
 	});
 	var textStyleLabels = new Style({
-            text: new Text({
-                declutter: true,
-                font: 'bold 12px "Roboto", "Arial Unicode MS", "sans-serif"',
-                placement: 'line',
-                scale: 1,
-                fill: new Fill({
-                    color: '#0D534D',
-                }),
-                stroke: new Stroke({
-                    color: 'white',
-                    width: 2
-                })
-            }),
-            stroke: new Stroke({
-                color: '#1E3765',
-                width: 1.5
-            })
+			text: new Text({
+				declutter: true,
+				font: 'bold 12px "Roboto", "Arial Unicode MS", "sans-serif"',
+				placement: 'line',
+				scale: 1,
+				fill: new Fill({
+					color: '#0D534D',
+				}),
+				stroke: new Stroke({
+					color: 'white',
+					width: 2
+				})
+			}),
+			stroke: new Stroke({
+				color: '#1E3765',
+				width: 1.5
+			})
 		});
 	var streetLabelLayer = new VectorLayer({
 		declutter: true,
 		source: vectorSource,
-        maxZoom: 15,
+		maxZoom: 15,
 		minZoom: 13,
 		style: function (feature) {
 			textStyleLabels.getText().setText(feature.get('n'));
-        	return textStyleLabels;
-      	},		
+			return textStyleLabels;
+	  	},		
 	});
 
 
-    
+	
 
 
-    // neighbourhoods
-    var features = new GeoJSON().readFeatures(neighbourhoods, {
+	// neighbourhoods
+	var features = new GeoJSON().readFeatures(neighbourhoods, {
 		});
 	var vectorSource = new VectorSource({
 		features
 	});
-    const neighbourhoodStyles = function(feature) {
-        let color = "grey";
-        if (feature.get('AREA_SHORT_CODE') >= 50) {color = '#0d534d' ;}
-        else if (feature.get('AREA_SHORT_CODE') >= 30 ){color ='#5b8985';}
-        else if (feature.get('AREA_SHORT_CODE') >= 10 ){color ='#a0b9b7';}
-        else if (feature.get('AREA_SHORT_CODE') >= -10 ){color ='#f2f2f2';}
-        else if (feature.get('AREA_SHORT_CODE') >= -30 ){color ='#f1da71';}
-        else if (feature.get('AREA_SHORT_CODE') >= -50 ){color ='#f1c500';}
-        else {color = '#d18100';}
+	const neighbourhoodStyles = function(feature) {
+		let color = "grey";
+		if (feature.get('AREA_SHORT_CODE') >= 50) {color = '#0d534d' ;}
+		else if (feature.get('AREA_SHORT_CODE') >= 30 ){color ='#5b8985';}
+		else if (feature.get('AREA_SHORT_CODE') >= 10 ){color ='#a0b9b7';}
+		else if (feature.get('AREA_SHORT_CODE') >= -10 ){color ='#f2f2f2';}
+		else if (feature.get('AREA_SHORT_CODE') >= -30 ){color ='#f1da71';}
+		else if (feature.get('AREA_SHORT_CODE') >= -50 ){color ='#f1c500';}
+		else {color = '#d18100';}
 
-            return  [
-            new Style({
-            stroke: new Stroke({
-                color: 'white',
-                width: 1,
-            }),
-            fill: new Fill({
-                color: color,
-            }),
-            }),
-        ]
-        };
-    var neighbourhoodsLayer = new VectorLayer({
+			return  [
+			new Style({
+			stroke: new Stroke({
+				color: 'white',
+				width: 1,
+			}),
+			fill: new Fill({
+				color: color,
+			}),
+			}),
+		]
+		};
+	var neighbourhoodsLayer = new VectorLayer({
 		// declutter: true,
 		source: vectorSource,
-        style: neighbourhoodStyles
+		style: neighbourhoodStyles
 	});
-    neighbourhoodsLayer.setOpacity(neighbourhoodOpacity);
+	neighbourhoodsLayer.setOpacity(neighbourhoodOpacity);
 
 
-    var features = new GeoJSON().readFeatures(notToronto, {
+	var features = new GeoJSON().readFeatures(notToronto, {
 		});
 	var vectorSource = new VectorSource({
 		features
@@ -202,62 +202,62 @@
 		style: style
 	});
 
-    // shoreline layer
-    var features = new GeoJSON().readFeatures(shoreline, {
+	// shoreline layer
+	var features = new GeoJSON().readFeatures(shoreline, {
 		});
 	var vectorSource = new VectorSource({
 		features
 	});
 	var shorelineStyle= new Style({
-        stroke: new Stroke({
-            color: '#1E3765',
-            width: 0.5
-        })
-    });
+		stroke: new Stroke({
+			color: '#1E3765',
+			width: 0.5
+		})
+	});
 	var shorelineLayer = new VectorLayer({
 		// declutter: true,
 		source: vectorSource,
 		style: function (feature) {
-        	return shorelineStyle;
-      	},		
+			return shorelineStyle;
+	  	},		
 	});
 
 
-    // six area labels
-    var features = new GeoJSON().readFeatures(sixLabels, {
+	// six area labels
+	var features = new GeoJSON().readFeatures(sixLabels, {
 		});
 	var vectorSource = new VectorSource({
 		features
 	});
 	var textSixStyleLabels = new Style({
-            text: new Text({
-                declutter: true,
-                font: '17px "TradeGothicBold"',
-                placement: 'point',
-                scale: 1,
-                fill: new Fill({
-                    color: '#1E3765',
-                }),
-                stroke: new Stroke({
-                    color: 'white',
-                    width: 2
-                })
-            })
+			text: new Text({
+				declutter: true,
+				font: '17px "TradeGothicBold"',
+				placement: 'point',
+				scale: 1,
+				fill: new Fill({
+					color: '#1E3765',
+				}),
+				stroke: new Stroke({
+					color: 'white',
+					width: 2
+				})
+			})
 		});
 	var sixLabelLayer = new VectorLayer({
 		declutter: true,
 		source: vectorSource,
 		style: function (feature) {
 			textSixStyleLabels.getText().setText(feature.get('name'));
-        	return textSixStyleLabels;
-      	},		
+			return textSixStyleLabels;
+	  	},		
 	});
 
 
 
 
 
-    useGeographic();
+	useGeographic();
 	const resolutions = [];
 	const matrixIds = [];
 	const proj3857 = getProjection('EPSG:3857');
@@ -267,64 +267,54 @@
 		resolutions[i] = maxResolution / Math.pow(2, i);
 	}
 
-    
+	
 
-    onMount(() => {
+	onMount(() => {
 
-        load = 1;
+		load = 1;
 
-        map = new Map({
+		map = new Map({
 			target: 'map2',
 			layers: [smallStreetsLayer, streetLayer, streetLabelLayer, neighbourhoodsLayer,  notTorontoLayer, shorelineLayer, sixLabelLayer],
 			view: new View({
 				center: [-79.38,43.71],
-                rotation: 17 * Math.PI / 180,
+				rotation: 17 * Math.PI / 180,
 				zoom: 11.2,
 				maxZoom: 15.2,
 				minZoom: 10,
 				extent: [-79.8302,43.3046,-78.9160,44.0295],
 			}),
 			controls: defaultControls(),
-            interactions: defaultInteractions({
-                mouseWheelZoom: false,
-            }),
+			interactions: defaultInteractions({
+				mouseWheelZoom: false,
+			}),
 		})
-        
-        map.addControl(new ScaleLine({units: 'metric', maxWidth: 75}));
-    
-    });
-
-
-    // toggling the land cover layer by year
-
-
-    // toggle streets (2022) on and off
-
-
-    
-    
+		
+		map.addControl(new ScaleLine({units: 'metric', maxWidth: 75}));
+	
+	});
 
 </script>
 
 
 
 <div id="legend2">
-    <svg width="280px" height="70px">
-        <text id="legend-title" x="0" y="18">% Change in Forest (1954-2017)</text>
-        <rect x="240" y="25" width="39" height="15" fill="#0d534d" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
-        <rect x="200" y="25" width="39" height="15" fill="#5b8985" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
-        <rect x="160" y="25" width="39" height="15" fill="#a0b9b7" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
-        <rect x="120" y="25" width="39" height="15" fill="#f2f2f2" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
-        <rect x="80" y="25" width="39" height="15" fill="#f1da71" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
-        <rect x="40" y="25" width="39" height="15" fill="#f1c500" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
-        <rect x="0" y="25" width="39" height="15" fill="#d18100" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
-        <text id="legend-label" x="24" y="55">-50%</text>
-        <text id="legend-label" x="64" y="55">-30%</text>
-        <text id="legend-label" x="104" y="55">-10%</text>
-        <text id="legend-label" x="150" y="55">10%</text>
-        <text id="legend-label" x="190" y="55">30%</text>
-        <text id="legend-label" x="230" y="55">50%</text>
-    </svg>
+	<svg width="280px" height="70px">
+		<text id="legend-title" x="0" y="18">% Change in Forest (1954-2017)</text>
+		<rect x="240" y="25" width="39" height="15" fill="#0d534d" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
+		<rect x="200" y="25" width="39" height="15" fill="#5b8985" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
+		<rect x="160" y="25" width="39" height="15" fill="#a0b9b7" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
+		<rect x="120" y="25" width="39" height="15" fill="#f2f2f2" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
+		<rect x="80" y="25" width="39" height="15" fill="#f1da71" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
+		<rect x="40" y="25" width="39" height="15" fill="#f1c500" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
+		<rect x="0" y="25" width="39" height="15" fill="#d18100" stroke="#FFFFFF" fill-opacity={neighbourhoodOpacity} stroke-width="1"/>
+		<text id="legend-label" x="24" y="55">-50%</text>
+		<text id="legend-label" x="64" y="55">-30%</text>
+		<text id="legend-label" x="104" y="55">-10%</text>
+		<text id="legend-label" x="150" y="55">10%</text>
+		<text id="legend-label" x="190" y="55">30%</text>
+		<text id="legend-label" x="230" y="55">50%</text>
+	</svg>
 </div>
 
 <div id="map2">
@@ -335,43 +325,43 @@
 
 <style>
 
-    #map2 {
-        margin: 0 auto;
-        max-width: 1000px;
-        width: 100%;
-        max-height: calc(100vh - 200px);
-        height: 500px;
-        border-bottom: solid 1px var(--brandDarkBlue);
-        border-top: solid 1px var(--brandDarkBlue);
-        width: 100%;
-        z-index: 3;
-        cursor: move;
-    }
+	#map2 {
+		margin: 0 auto;
+		max-width: 1000px;
+		width: 100%;
+		max-height: calc(100vh - 200px);
+		height: 500px;
+		border-bottom: solid 1px var(--brandDarkBlue);
+		border-top: solid 1px var(--brandDarkBlue);
+		width: 100%;
+		z-index: 3;
+		cursor: move;
+	}
 
-    #legend2 {
-        margin: 0 auto;
-        margin-top: 10px;
-        margin-bottom: 10px;
-        width: 280px;
-        height: 60px;
-        padding: 0px;
-    }
+	#legend2 {
+		margin: 0 auto;
+		margin-top: 10px;
+		margin-bottom: 10px;
+		width: 280px;
+		height: 60px;
+		padding: 0px;
+	}
 
-    #legend-title {
-        font-family: 'TradeGothicBold', sans-serif;
-        font-weight: normal;
-        height: 25px;
-        font-size: 20px;
-        background-color:  var(--brandWhite);
-        fill: var(--brandDarkBlue);
-    }
+	#legend-title {
+		font-family: 'TradeGothicBold', sans-serif;
+		font-weight: normal;
+		height: 25px;
+		font-size: 20px;
+		background-color:  var(--brandWhite);
+		fill: var(--brandDarkBlue);
+	}
 
-    #legend-label {
-        font-family: 'Roboto', sans-serif;
-        font-weight: normal;
-        font-size: 15px;
-        background-color:  var(--brandWhite);
-        fill: var(--brandDarkBlue);
-    }
+	#legend-label {
+		font-family: 'Roboto', sans-serif;
+		font-weight: normal;
+		font-size: 15px;
+		background-color:  var(--brandWhite);
+		fill: var(--brandDarkBlue);
+	}
 
 </style>
--- src/lib/SankeyChart.svelte
+++ src/lib/SankeyChart.svelte
@@ -4,23 +4,23 @@
 </script>
 
 <div class="graph-title">
-    Toronto Land Cover Transitions (1939-2017)
+    Toronto Land Cover Transitions
 </div>
 
 <div id="legend-ribbon">
-    <svg width="430px" height="25px">
-        <rect x="360" y="0" width="70" height="25" fill="#FFFFFF" stroke="#E1E1E1" stroke-width="1"/>
-        <rect x="288" y="0" width="70" height="25" fill="#D0D1C9" stroke="#FFFFFF" stroke-width="1"/>
-        <rect x="216" y="0" width="70" height="25" fill="#6FC7EA" stroke="#FFFFFF" stroke-width="1"/>
-        <rect x="144" y="0" width="70" height="25" fill="#0D534D" stroke="#FFFFFF" stroke-width="1"/>
-        <rect x="72" y="0" width="70" height="25" fill="#00A189" stroke="#FFFFFF" stroke-width="1"/>
-        <rect x="0" y="0" width="70" height="25" fill="#f1c500" stroke="#FFFFFF" stroke-width="1"/>
-        <text id="legend-label" x="22" y="17">Built</text>
-        <text id="legend-label-dark" x="92" y="17">Grass</text>
-        <text id="legend-label-dark"x="162" y="17">Forest</text>
-        <text id="legend-label" x="234" y="17">Water</text>
-        <text id="legend-label" x="310" y="17">Bare</text>
-        <text id="legend-label" x="372" y="17">No Data</text>
+    <svg width="360px" height="25px">
+        <rect x="300" y="0" width="60" height="25" fill="#FFFFFF" stroke="#E1E1E1" stroke-width="1"/>
+        <rect x="240" y="0" width="60" height="25" fill="#D0D1C9" stroke="#FFFFFF" stroke-width="1"/>
+        <rect x="180" y="0" width="60" height="25" fill="#6FC7EA" stroke="#FFFFFF" stroke-width="1"/>
+        <rect x="120" y="0" width="60" height="25" fill="#0D534D" stroke="#FFFFFF" stroke-width="1"/>
+        <rect x="60" y="0" width="60" height="25" fill="#00A189" stroke="#FFFFFF" stroke-width="1"/>
+        <rect x="0" y="0" width="60" height="25" fill="#f1c500" stroke="#FFFFFF" stroke-width="1"/>
+        <text id="legend-label" x="30" y="17" text-anchor="middle">Built</text>
+        <text id="legend-label-dark" x="90" y="17" text-anchor="middle">Grass</text>
+        <text id="legend-label-dark"x="150" y="17" text-anchor="middle">Forest</text>
+        <text id="legend-label" x="210" y="17" text-anchor="middle">Water</text>
+        <text id="legend-label" x="270" y="17" text-anchor="middle">Bare</text>
+        <text id="legend-label" x="330" y="17" text-anchor="middle">No Data</text>
     </svg>
 </div>
 
@@ -34,10 +34,13 @@
     margin: 0 auto;
     margin-top: 10px;
     margin-bottom: 10px;
-    width: 567px;
-    height: 302px;
+    width: 600px;
     padding-left: 0px;
     padding-right: 0px;
+    border-bottom: solid 1px var(--brandDarkBlue);
+}
+img {
+    width: 100%
 }
 
 .graph-title {
@@ -45,14 +48,15 @@
     font-size: 30px;
     font-family: 'TradeGothicBold', sans-serif;
     font-weight: normal;
-    height: 40px;
+    height: 20px;
     font-size: 20px;
     color: var(--brandDarkBlue);
     background-color: var(--brandWhite);
+    border-top: solid 1px var(--brandDarkBlue);
+    padding-top: 10px;
     max-width: 600px;
     margin: 0 auto;
-    padding-left: 15px;
-    padding-right: 15px;
+    padding-left: 0px;
     margin-bottom: 0px;
     position: relative;
 }
@@ -61,9 +65,10 @@
         margin: 0 auto;
         margin-top: 10px;
         margin-bottom: 10px;
-        width: 430px;
+        width: 360px;
         height: 30px;
         padding: 0px;
+       
     }
 
 #legend-label {
