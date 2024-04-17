68ec8ef4905cee350b42bf751eec28cd815844df
Jeff Allen <jeffallen89@gmail.com>
2024-04-17 10:47:40 -0400

    text edit, rebuild

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -62,7 +62,7 @@
 			<a href="https://francoisecardou.wixsite.com/site" target="_blank">Françoise Cardou</a>, <a href="https://github.com/nsookhan" target="_blank">Nicholas Sookhan</a>, <a href="https://jamaps.github.io/" target="_blank">Jeff Allen</a>, <a href="https://www.researchgate.net/profile/Daniel-Silver-4" target="_blank">Dan Silver</a>, <a href="https://www.macivorlab.ca/" target="_blank">Scott MacIvor</a>, <a href="https://cubes-labs.com/" target="_blank">Marc Cadotte</a>
 			<br>
 			<br>
-			Funded by a <a href="https://schoolofcities.utoronto.ca/programs-opportunities/urban-challenge-grant/" target="blank">School of Cities Urban Challenge Grant</a>, Toronto and Region Conservation Authority, and Mitacs.
+			Funded by a <a href="https://schoolofcities.utoronto.ca/programs-opportunities/urban-challenge-grant/" target="blank">School of Cities Urban Challenge Grant</a>.
 		</p>
 
 	</div>
@@ -73,7 +73,7 @@
 			If you were headed home to one of Toronto’s downtown neighbourhoods after a walk in the Greenbelt on a hot summer day, you would most likely also brace for the heat of the city core. Because concrete traps heat during the day and releases it during the night, cities tend to be several degrees warmer than surrounding natural areas. Though this temperature difference is small, it is enough to affect the health and wellbeing of those who live in the city. 
 		</p>
 		<p>
-			Trees, whether along streets or in forest fragments, can help. They reflect sunlight, provide shade and convert heat to water vapour. However, benefits from this cooling require one lives and/or works near greenspaces - the closer the better. What happens to neighbourhoods that do not have ready access to green spaces? With climate change, we are experiencing longer and more intense summer heat waves, and it is critical we understand the many ways in which access to nature will shape city life in the future. One way we can begin to do this is by first looking into the past. 
+			Trees, whether along streets or in forest fragments, can help. They reflect sunlight, provide shade and convert heat to water vapour. However, benefits from this cooling require that one live and/or work near greenspaces - the closer the better. What happens to neighbourhoods that do not have ready access to green spaces? With climate change, we are experiencing longer and more intense summer heat waves, and it is critical we understand the many ways in which access to nature will shape city life in the future. One way we can begin to do this is by first looking into the past. 
 		</p>
 		
 	</div>
@@ -94,10 +94,10 @@
 			Nowadays, satellites provide detailed imagery covering the full spectrum of colours we can see, and some we can’t, making it easier to map natural areas. Yet satellite imaging is still a relatively new technology with the oldest images from the late 70s, long after the establishment and growth of most north american cities. Luckily, aeroplanes have been around for longer, and we have been flying airborne cameras to document city shape going back to the 1930s. Taken from a low altitude, these black and white photographs contain a treasure trove of information, but it needs to be extracted. 
 		</p>
 		<p>
-			Going from a black and white images to land cover categories (whether a particular area is wooded, grassy, built-up, bare or, well, watery) has up until now involved painstaking visual interpretation by experts - a time-consuming task that limits the number of photographs that can be analysed. We developed a model that breaks down images into smaller areas of manageable size and identifies wooded areas, grass, built up land, bare ground and water bodies in a fraction of the time. 
+			Going from black and white images to land cover categories (whether a particular area is wooded, grassy, built-up, bare or, well, watery) has up until now involved painstaking visual interpretation by experts - a time-consuming task that limits the number of photographs that can be analysed. We developed a model that breaks down images into smaller areas of manageable size and identifies wooded areas, grass, built up land, bare ground and water bodies in a fraction of the time. 
 		</p>
 		<p>
-			Convolutional neural networks (CNN) are a state-of-the-art technique used in image analysis that generate a label for each pixel in input imagery. To achieve accurate label individual pixels, we trained a CNN model using a subset of historical imagery for which we manually mapped land cover. The process of training is called supervised learning. This is because during training, the CNN model learns the pixel properties of input imagery that relate to the user-provided categories (e.g. manually mapped land cover associated with an input aerial photograph). Following training, our CNN model provides accurate and scalable classification of wooded areas, grass, built up land, bare ground and water bodies in input aerial photography. We then deployed our CNN model to map the whole city of Toronto from a <a href="https://schoolofcities.github.io/historical-aerial-imagery-toronto/" target="_blank">series of aerial photographs</a> spanning decades in a relatively short time, allowing us to quantify citywide change over time.
+			Convolutional neural networks (CNN) are a state-of-the-art technique used in image analysis that generate a label for each pixel in input imagery. To accurately label individual pixels, we trained a CNN model using a subset of historical imagery for which we manually mapped land cover. The process of training is called supervised learning. This is because during training, the CNN model learns the pixel properties of input imagery that relate to the user-provided categories (e.g. manually mapped land cover associated with an input aerial photograph). Following training, our CNN model provides accurate and scalable classification of wooded areas, grass, built up land, bare ground and water bodies in input aerial photography. We then deployed our CNN model to map the whole city of Toronto from a <a href="https://schoolofcities.github.io/historical-aerial-imagery-toronto/" target="_blank">series of aerial photographs</a> spanning decades, allowing us to quantify citywide change in a relatively short time.
 		</p>
 	</div>
 
@@ -105,7 +105,7 @@
 
 	<div class="text">
 		<p>
-			We found that since 1939, Toronto development has gone through two distinct phases, from expansion (1950-1973) to infill (1970-1990). During the postwar period, agricultural land was rapidly converted into largely residential developments, reaching an average rate of <b>~1800 ha/yr between 1954 and 1973</b>. During this time neighbourhoods including Don Mills and Flemingdon Park were developed, and there was significant residential expansion in Etobicoke and North York. In comparison, the infill period saw the development of small but important remnant spaces within the city matrix, resulting in the loss of many nearby green spaces. 
+			Since 1939, Toronto development has gone through two distinct phases, from expansion (1950-1973) to infill (1970-1990). During the postwar period, agricultural land was rapidly converted into largely residential developments, reaching an average rate of <b>~1800 ha/yr between 1954 and 1973</b>. During this time neighbourhoods including Don Mills and Flemingdon Park were developed, and there was significant residential expansion in Etobicoke and North York. In comparison, the infill period saw the development of small but important remnant spaces within the city matrix, resulting in the loss of many nearby green spaces. 
 
 		</p>
 		<p>
